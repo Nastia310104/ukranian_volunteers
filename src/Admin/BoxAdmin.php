8a882085f2b1c1968329e4e21671ced858be7e21
Nastia <77219785+Nastia310104@users.noreply.github.com>
2022-12-14 20:17:34 -0500

    Revert "Revert "Feature/vb 7 create box admin""

---
+++ src/Admin/BoxAdmin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Admin;
+
+use App\Entity\Category;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\Form\Type\CollectionType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class BoxAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->with('group.general', ['class' => 'col-md-6'])
+                ->add('id', null, [
+                    'required' => false,
+                    'disabled' => true,
+                ])
+                ->add('pallet', null, [
+                    'required' => true,
+                ])
+                ->add('receiver', null, [
+                    'required' => true,
+                ])
+            ->end()
+            ->with('group.categories', ['class' => 'col-md-6'])
+                ->add('categories', EntityType::class, [
+                    'class' => Category::class,
+                    'choice_label' => 'categoryName',
+                    'multiple' => true,
+                ])
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('pallet')
+            ->add('receiver')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->addIdentifier('id')
+            ->add('pallet')
+            ->add('receiver')
+            ->add('categories')
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->with('group.general', ['class' => 'col-md-6'])
+                ->add('id')
+                ->add('pallet')
+                ->add('receiver')
+                ->add('categories')
+            ->end()
+        ;
+    }
+}
